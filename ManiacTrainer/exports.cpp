2ac4335da65ceec6a6a3f4c81c7e586fd55d04d7
ManiacKnight <morevideogames20@gmail.com>
2024-01-19 14:53:26 +0530

    Revamped Files

--- ManiacTrainer/exports.cpp
+++ ManiacTrainer/exports.cpp
@@ -1,7 +1,10 @@
 #include "INIReader.h"
 
+//Dummy Export to add to the Game's IAT (Import Address Table)
 int __declspec(dllexport) DummyFunction() { return 0; }
 
+//Custom Function written to be called from inside the game's code.
+//Some trainer functions have been patched directly into the game as assembly code, but still need access to the INI values to turn them on or off.
 int getConfigValue(const char* KEY, const char* SECTION)
 {
 	INIReader ini("Config.ini");
